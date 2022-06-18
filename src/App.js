8cb2130550d88737aa9ac2f75ebae3a0fcc92338
KainiChang <changkaini91@gmail.com>
2022-06-18 16:27:08 +0930

    submit display list by map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const names=["Janmes","Kaini","Johney","David"]
+  console.log(names)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    names.map((item)=>   
+    <p>
+     {item}
+    </p>
+  ))
 }
 
 export default App;
